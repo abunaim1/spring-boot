0f8ceac00b681e5a0e56c78220c65591011187f3
Abu Naim <aabunnaim@gmail.com>
2026-02-21 23:35:17 +0600

    Removed unused files

--- internalWorkingOfSpringBoot 2/src/main/java/com/shadhinlab/internalWorkingOfSpringBoot/RazorpayPaymentService.java
+++
@@ -1,4 +0,0 @@
-package com.shadhinlab.internalWorkingOfSpringBoot;
-
-public class RazorpayPaymentService {
-}
--- internalWorkingOfSpringBoot/src/main/java/com/shadhinlab/internalWorkingOfSpringBoot/PaymentService.java
+++
@@ -1,5 +0,0 @@
-package com.shadhinlab.internalWorkingOfSpringBoot;
-
-public interface PaymentService {
-    String pay();
-}
